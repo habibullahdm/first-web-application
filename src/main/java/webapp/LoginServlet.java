8b6d4ffcb8a170e3598c1faffdc29b5e992c9490
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2026-08-22 13:36:44 +0700

    step 7: add password field and logic login

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
-    @Override
+	private final LoginService service = new LoginService();
+
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
@@ -19,7 +21,18 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		request.setAttribute("name", request.getParameter("name"));
-		request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
+		boolean isValidUser = service.validateUser(name, password);
+
+		if (isValidUser) {
+			request.setAttribute("name", name);
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+		} else {
+			request.setAttribute("errorMessage", "Invalid Credentials!!");
+			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
 	}
+
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,12 +1,18 @@
+<%--@elvariable id="errorMessage" type=""--%>
 <html>
 <head>
     <title>Yahoo!!</title>
 </head>
 <body>
+<p><font color="red">${errorMessage}</font></p>
 <form action="/login.do" method="POST">
     Name : <label>
     <input name="name" type="text"/>
-</label> <input type="submit"/>
+</label>
+    Password : <label>
+    <input name="password" type="password"/>
+</label>
+    <input type="submit"/>
 </form>
 </body>
 </html>
