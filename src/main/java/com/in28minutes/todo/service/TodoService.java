4051eb66896d9160387b99c9d58eb33daf489b3d
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2026-08-22 15:30:58 +0700

    step 18: add todo service

---
+++ src/main/java/com/in28minutes/todo/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.in28minutes.todo.service;
+
+import com.in28minutes.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TodoService {
+	private static final List<Todo> todos = new ArrayList<Todo>();
+	private static int todoCount = 3;
+
+	static {
+		todos.add(new Todo(1, "in28Minutes", "Learn Spring MVC", new Date(),
+				false));
+		todos.add(new Todo(2, "in28Minutes", "Learn Struts", new Date(), false));
+		todos.add(new Todo(3, "in28Minutes", "Learn Hibernate", new Date(),
+				false));
+	}
+
+	public List<Todo> retrieveTodos(String user) {
+		List<Todo> filteredTodos = new ArrayList<Todo>();
+		for (Todo todo : todos) {
+			if (todo.getUser().equals(user))
+				filteredTodos.add(todo);
+		}
+		return filteredTodos;
+	}
+
+	public void addTodo(String name, String desc, Date targetDate, boolean isDone) {
+		todos.add(new Todo(++todoCount, name, desc, targetDate, isDone));
+	}
+
+	public void deleteTodo(int id) {
+        todos.removeIf(todo -> todo.getId() == id);
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/list-todos.jsp
@@ -0,0 +1,11 @@
+<%--@elvariable id="todos" type=""--%>
+<%--@elvariable id="name" type=""--%>
+<html>
+<head>
+    <title>Todos for ${name}</title>
+</head>
+<body>
+<H1>Your Todos</H1>
+${todos}
+</body>
+</html>
