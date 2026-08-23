4101ae2f67c85a24a30d86eebacdc732eb6f527f
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2026-08-23 17:52:29 +0700

    step 37: add path param

--- src/main/java/com/in28minutes/controller/TodoRestController.java
+++ src/main/java/com/in28minutes/controller/TodoRestController.java
@@ -3,6 +3,7 @@ package com.in28minutes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class TodoRestController {
 	@RequestMapping(value = "/todo/", method = RequestMethod.GET)
 	public List<Todo> listAllTodos() {
         return service.retrieveTodos("in28Minutes");
+	}
+
+	@RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
+	public Todo retrieveTodo(@PathVariable("id") int id) {
+		return service.retrieveTodo(id);
 	}
 
 }
